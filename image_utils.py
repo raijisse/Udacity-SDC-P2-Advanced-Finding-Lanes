f9e1bdef8126e0cd94f19bc180abca41738e9d7c
Regis <regis.rauzy@continental-corporation.com>
2019-04-06 00:22:12 +0200

    upload output video

--- image_utils.py
+++ image_utils.py
@@ -316,6 +316,36 @@ def search_around_poly(binary_warped, left_fit, right_fit):
 
     return  left_fitx, left_fit, right_fitx, right_fit
 
+
+def measure_curvature(left_fit_cr, right_fit_cr):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/800 # meters per pixel in x dimension
+        
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = 720
+    
+    # Calculation of R_curve (radius of curvature)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    
+    
+    # Calculate vehicle center
+    left_lane_bottom = (left_fit_cr[0]*y_eval)**2 + left_fit_cr[0]*y_eval + left_fit_cr[2]
+    right_lane_bottom = (right_fit_cr[0]*y_eval)**2 + right_fit_cr[0]*y_eval + right_fit_cr[2]
+    
+    # Computer lane center
+    lane_center = (left_lane_bottom + right_lane_bottom)/2.
+    center_image = 640
+    center = (lane_center-center_image)*xm_per_pix #Convert to meters
+    
+    return left_curverad, right_curverad, center
+
+
 def lane_back_to_image(image, warped_img, left_fitx, right_fitx, ploty, Minv, left_curvature, right_curvature, center):
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(warped_img).astype(np.uint8)
